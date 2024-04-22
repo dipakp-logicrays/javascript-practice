25ea8d3bb67dd4a1d208bdddf5ffe1ddc0aaa560
dipakp-logicrays <dipakp@logicrays.com>
2024-04-22 18:41:42 +0530

    Add boolean examples

---
+++ Examples/10.boolean.js
@@ -0,0 +1,34 @@
+// Reference: https://www.w3schools.com/js/js_booleans.asp
+
+// Very often, in programming, you will need a data type that can only have one of two values, like
+	1. YES / NO
+	2. ON / OFF
+	3. TRUE / FALSE
+
+// Everything With a "Value" is True
+	Boolean(100);
+	Boolean(3.14)
+	Boolean(-15)
+	Boolean("Hello")
+	Boolean('false')
+	Boolean(1 + 7 + 3.14)
+
+// Everything Without a "Value" is False
+	1. The Boolean value of 0 (zero) is false:
+		let x = 0;
+		Boolean(x);
+
+	2. The Boolean value of -0 (minus zero) is false:
+
+	3. The Boolean value of "" (empty string) is false:
+
+	4. The Boolean value of undefined is false:
+		let x;
+		Boolean(x);
+
+	5. The Boolean value of null is false:
+
+	6. The Boolean value of false is (you guessed it) false:
+
+	7. The Boolean value of NaN is false:
+
